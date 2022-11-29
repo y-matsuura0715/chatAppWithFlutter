70f37ac28c96a34ccfba3bf2023abf56f3eb9886
y-matsuura0715 <y-matsuura0715@outlook.jp>
2022-11-29 22:32:10 +0900

    ローカルでコミット5000

--- lib/presentation/view/chat.dart
+++ lib/presentation/view/chat.dart
@@ -10,7 +10,7 @@ class Chat extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.deepPurpleAccent,
-        title: const Text('リモートでコミット'),
+        title: const Text('ローカルでコミット500'),
       ),
       body: Center(
         child: GridView.builder(
