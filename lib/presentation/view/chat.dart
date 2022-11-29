3497a31070243df192c750eb2b0293e0b49c38f5
y-matsuura0715 <y-matsuura0715@outlook.jp>
2022-11-29 22:26:14 +0900

    コンフリクト解消

--- lib/presentation/view/chat.dart
+++ lib/presentation/view/chat.dart
@@ -10,7 +10,7 @@ class Chat extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.deepPurpleAccent,
-        title: const Text('リモートでコミット'),
+        title: const Text('commit済み'),
       ),
       body: Center(
         child: GridView.builder(
